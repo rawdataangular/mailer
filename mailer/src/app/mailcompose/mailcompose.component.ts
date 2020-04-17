f9e0965c51f9f221126d8a3945454702c5784cc7
binush@rawdatatech.com <binush@rawdatatech.com>
2020-04-17 15:50:34 +0530

    Add smtpjs with static data

--- mailer/src/app/mailcompose/mailcompose.component.ts
+++ mailer/src/app/mailcompose/mailcompose.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { empty } from 'rxjs';
+declare const Email: any;
 
 @Component({
   selector: 'app-mailcompose',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailComposeComponent implements OnInit {
 
+  @ViewChild('email_address',{static: true}) email_address: ElementRef;
+  @ViewChild('email_subject',{static: true}) email_subject: ElementRef;
+  @ViewChild('email_message',{static: true}) email_message: ElementRef;
+
+  
+  Email: any
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+
+    if(this.email_message.nativeElement.value===empty){
+      this.email_message.nativeElement.value="Good wishes from team RawData"
+    }
+    if(this.email_subject.nativeElement.value===empty){
+      this.email_subject.nativeElement.value="Wishes"
+    }
+ 
+      
+      Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "indu@rawdatatech.com",
+        Password : "6A22260680F50D0E36C024E77F11A5B8EFC2",
+        To : "binush@rawdatatech.com",
+        From : "indu@rawdatatech.com",
+        Subject : "this is subject",
+        Body : "this is message"
+    }).then(
+      message => alert(message)
+    )
+    
   }
 
 }
